0dec34617d3453fbb87a34a6a845c14231b22ed1
LenyPython <buniadyserv@gmail.com>
2021-08-06 20:40:47 +0200

    added functionality to timer component, created timer 1 sec interval for counting down session time and breaks

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,13 +1,61 @@
 import React from 'react'
+import './timer.css'
 
 
-const Timer = ({timeLeft}) => {
+const Timer = ({pause, session}) => {
+  let sessionDuration = session
+  let breakDuration = pause
+  let seconds = 0
+  let sessionTime = true
+
+  const handlePlayPause = event => {
+    let command = event.target.innerText
+    if (command === 'Play >>>') {
+      event.target.innerText = 'Pause ||'
+      var countDown = setInterval(() => {
+        if (sessionTime) {
+          if (seconds === 0) {
+            sessionDuration -= 1
+            seconds = 60
+          }
+          seconds -= 1
+          let timeLeft = `${sessionDuration < 10 ? '0' + sessionDuration : sessionDuration}:${seconds < 10 ? '0' + seconds : seconds}`
+          document.querySelector('#time-left').innerText = timeLeft
+          document.querySelector('#name-label').innerText = 'Session'
+          if (sessionDuration === 0 && seconds === 0) sessionTime = false
+        } else {
+          if (seconds === 0) {
+            breakDuration -= 1
+            seconds = 60
+          }
+          seconds -= 1
+          let timeLeft = `${breakDuration < 10 ? '0' + breakDuration : breakDuration}:${seconds < 10 ? '0' + seconds : seconds}`
+          document.querySelector('#time-left').innerText = timeLeft
+          document.querySelector('#name-label').innerText = 'Break Time'
+        }
+
+      }, 1000)
+    } else {
+      event.target.innerText = 'Play >>>'
+      clearInterval(countDown)
+    }
+  }
+
+  const handleReset = () => {
+    sessionDuration = session
+    breakDuration = pause
+    seconds = 0
+    sessionTime = true
+  }
+
   return (
     <div id='timer-label'>
-      <h2>Session</h2>
-      <p id='time-left'> {timeLeft} </p>
-      <button id='start_stop'>Play/Pause</button>
-      <button id='reset'>reset</button>
+      <h2 id='name-label'>Session</h2>
+      <p id='time-left'>
+        {`${sessionDuration < 10 ? '0' + sessionDuration : sessionDuration}:${seconds < 10 ? '0' + seconds : seconds}`}
+      </p>
+      <button id='start_stop' onClick={handlePlayPause}>Play >>></button>
+      <button id='reset' onClick={handleReset}>reset</button>
     </div>
   )
 }
