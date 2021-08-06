7d6cc5833ca3f96cc720a20c8eb89951d5fbdcc5
LenyPython <buniadyserv@gmail.com>
2021-08-06 11:57:04 +0200

    removed unused files. Added basic structure => timer and controls component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Work Break Time</h1>
       </header>
+      <Controls />
+      <Timer />
+      <footer>
+        <h2>Made by <span>Piotr Lenartowicz</span></h2>
+      </footer>
+
     </div>
   );
 }
